3f05691777127a0a7db685f46d2027a104e782e2
Joseph Carter <jcarter@lincoln.ne.gov>
2023-04-12 13:46:32 -0500

    Updated for loop to limit number of iterations to half.

--- Palindromes/Palindrome.js
+++ Palindromes/Palindrome.js
@@ -37,7 +37,7 @@ function VerifyPalindromes(){
 function Palindrominator(word){
     let Palindrome = true
     let letterArray = word.split("");
-    for (var i = 0; i < word.length; i++){
+    for (var i = 0; i < word.length/2; i++){
         if (letterArray[i] != letterArray[word.length-i-1]){
             Palindrome = false;
         }
